c2583cebe51fe7b86e19e08f0dbedde719f06209
Sunbertus <stjhingtio@gmail.com>
2024-12-17 11:48:59 +0700

    Second Commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Hero from './sections/Hero.jsx';
 import About from './sections/About.jsx';
 import Footer from './sections/Footer.jsx';
 import Navbar from './sections/Navbar.jsx';
-
+import Project from './sections/Project.jsx'
 import WorkExperience from './sections/Experience.jsx';
 
 const App = () => {
@@ -12,7 +12,9 @@ const App = () => {
       <Hero />
       <About />
       <WorkExperience />
+      <Project/>
       <Footer />
+   
     </main>
   );
 };
